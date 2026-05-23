15d957f23930b1faaa9af3409295d21e778884dc
pipepro06 <adrianfelipenavarro@gmail.com>
2026-05-23 10:39:30 -0500

    actividad api

---
+++ app-crud-api/src/services/productService.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+const BASE_URL = 'https://6a11b0523e35d0f37ee38ccb.mockapi.io'
+const RESOURCE = `${BASE_URL}/products`
+
+export default {
+  getAllProducts() {
+    return axios.get(RESOURCE)
+  },
+
+  getProductById(id) {
+    return axios.get(`${RESOURCE}/${id}`)
+  },
+
+  createProduct(data) {
+    return axios.post(RESOURCE, data)
+  },
+
+  updateProduct(id, data) {
+    return axios.put(`${RESOURCE}/${id}`, data)
+  },
+
+  deleteProduct(id) {
+    return axios.delete(`${RESOURCE}/${id}`)
+  }
+}
